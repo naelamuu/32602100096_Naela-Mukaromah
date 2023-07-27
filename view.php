4035b9792a432d4c4a684faf5da95350c805f12a
sabilhuda87 <140683265+sabilhuda87@users.noreply.github.com>
2023-07-27 11:42:19 +0700

    Upload view.php

---
+++ view.php
@@ -0,0 +1,47 @@
+<?php $this->extend('template'); ?>
+<?php $this->section('content'); ?>
+
+
+<main>
+     <div class="title">
+          <h1>Data Mahasiswa</h1>
+          <a href="crud/tambah"><button>Tambah data</button></a>
+     </div>
+     <div class="table">
+          <table border="1" cellspacing="0" cellpadding="10">
+               <tr>
+                    <th>NO</th>
+                    <th>Nama</th>
+                    <th>NIM</th>
+                    <th>Prodi</th>
+                    <th>Universitas</th>
+                    <th>NO HP</th>
+                    <th>Action</th>
+               </tr>
+               <?php 
+               if (empty($mahasiswa)) { ?>
+               <tr>
+                    <td colspan="7">Tidak ada data</td>
+               </tr>
+               <?php } else {
+               $i = 1;
+               foreach ($mahasiswa as $a) { ?>
+               <tr>
+                    <td><?= $i++; ?></td>
+                    <td><?= $a['nama']; ?></td>
+                    <td><?= $a['nim']; ?></td>
+                    <td><?= $a['prodi']; ?></td>
+                    <td><?= $a['universitas']; ?></td>
+                    <td><?= $a['nomor']; ?></td>
+                    <td class="action">
+                         <a href="crud/hapus/<?= $a['nim']; ?>"><button class="btn-delete" onclick="return confirm('Are you sure to delete NIM <?= $a['nim'] ?> ?')">Delete</button></a>
+                         <a href="crud/edit/<?= $a['nim']; ?>"><button class="btn-update">Update</button></a>
+                    </td>
+               </tr>
+               <?php }
+               } ?>
+          </table>
+     </div>
+</main>
+
+<?php $this->endsection(); ?>
