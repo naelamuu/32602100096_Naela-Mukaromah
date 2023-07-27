c3a4ff4d148a5330b3bb1b83de9eef6fd22e614e
Naela Mukaromah <90626937+naelamuu@users.noreply.github.com>
2023-07-27 11:40:54 +0700

    upload template.php

---
+++ template.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="/CSS/crud.css">
+     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.4.0/css/all.css">
+     <title>CRUD</title>
+</head>
+
+<body>
+     <header>
+          <nav>
+               <table>
+                    <tr>
+                         <td>Nama</td>
+                         <td> : </td>
+                         <td>Naela /Muslikah /Sabil /Praktikum Web Programming</td>
+                    </tr>
+                    <tr>
+                         <td>NIM</td>
+                         <td> : </td>
+                         <td>32602100096 /32602100094 /32602100087</td>
+                    </tr>
+               </table>
+          </nav>
+     </header>
+     <?php $this->rendersection('content')?>
+</body>
+
+</html>
